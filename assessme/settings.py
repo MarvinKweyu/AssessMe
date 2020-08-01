5b30029ad54c5793d225790e88edb733a976c0ce
MarvinKweyu <mkweyu1@gmail.com>
2020-08-01 14:09:40 +0300

    static files re-collection

--- assessme/settings.py
+++ assessme/settings.py
@@ -111,9 +111,9 @@ STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 # Custom Django auth settings
