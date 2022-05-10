46a80f439defbfe8220fe0988f84b2e8cf41a9e4
Marvin Kweyu <mkweyu1@gmail.com>
2022-05-10 16:50:11 +0300

    chore: return allowed hosts

--- assessme/settings/prod.py
+++ assessme/settings/prod.py
@@ -8,9 +8,8 @@ load_dotenv()
 # since it's running on my machine, show me the errors
 DEBUG = False
 
-# once heroku fixes automatic deployment, we can use this
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
-ALLOWED_HOSTS = ["assessme-project.herokuapp.com"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+
 
 DATABASES = {}
 DATABASES["default"] = dj_database_url.config(conn_max_age=600)
